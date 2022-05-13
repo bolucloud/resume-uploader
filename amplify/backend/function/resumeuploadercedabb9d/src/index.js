ad67d3ae9d9022e0f27294cf7a2d38221a10f1c4
bolucloud <bolu@bolu.cloud>
2022-05-12 21:13:03 -0500

    add backend

---
+++ amplify/backend/function/resumeuploadercedabb9d/src/index.js
@@ -0,0 +1,60 @@
+/* Amplify Params - DO NOT EDIT
+	ENV
+	REGION
+	STORAGE_RESUMEUPLOADER_BUCKETNAME
+	SES_EMAIL
+Amplify Params - DO NOT EDIT */
+
+const aws = require("aws-sdk");
+const nodemailer = require("nodemailer");
+const ses = new aws.SES();
+const s3 = new aws.S3();
+const transporter = nodemailer.createTransport({
+  SES: { ses, aws },
+});
+
+exports.handler = async (event) => {
+  //eslint-disable-line
+
+  for (const streamedItem of event.Records) {
+    if (streamedItem.eventName === "INSERT") {
+      const filename = streamedItem.dynamodb.NewImage.resumeFile.S;
+      const candidateEmail = streamedItem.dynamodb.NewImage.email.S;
+      const candidateName = streamedItem.dynamodb.NewImage.name.S;
+      const candidateIdentity = streamedItem.dynamodb.NewImage.userIdentity.S;
+
+      const RESUME_BUCKET =
+        process.env.resumeuploadere58cc4f1f78747f6bf99e18b0a1b72b7;
+
+      try {
+        // get record from s3
+        const resumeFile = await s3
+          .getObject({
+            Bucket: RESUME_BUCKET,
+            Key: `protected/${candidateIdentity}/${filename}`,
+          })
+          .promise();
+
+        // email with attachment
+        const mailOptions = {
+          from: process.env.SES_EMAIL,
+          subject: "Candidate Resume Submission",
+          html: `<p>You can reach ${candidateName} at the following email: <b>${candidateEmail}</b></p>`,
+          to: process.env.SES_EMAIL,
+          attachments: [
+            {
+              filename,
+              content: resumeFile.Body,
+            },
+          ],
+        };
+
+        // send email with transporter
+        await transporter.sendMail(mailOptions);
+      } catch (e) {
+        console.error("Error", e);
+      }
+    }
+  }
+  return { status: "done" };
+};
--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 import "./App.css";
 
 function App() {
-  return <div className="App">Fresh start</div>;
+  return (
+    <div className="App">
+      <Container fluid>
+        <Row>
+          <Col>1 of 1</Col>
+        </Row>
+        <div>Hiring Now</div>
+        <div>Think you got what it takes to join the team?</div>
+        <div>Lorem ipsum bla bla bla </div>
+        <Button variant="primary">Upload Resume</Button>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
